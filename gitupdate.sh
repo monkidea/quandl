9595c2cb5b7685dc39ba3458fae6ebd4300cc612
Matthew <stidmatt@gmail.com>
2018-06-30 12:32:06 -0700

    Test commit

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,8 +1,6 @@
-echo Where do you want to put your files?
-read varname
-echo What do you want to call your commit?
+echo "What do you want to call your commit?"
 read varname2
 git init
 git add .
 git commit -m $varname2
-git push origin $varname
+git push origin
